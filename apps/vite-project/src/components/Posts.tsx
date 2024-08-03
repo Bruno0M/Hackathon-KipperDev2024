9aad4850e6e06a2554fca9c2d56a5f57238f5f90
Pyments <edupymota@protonmail.com>
2024-08-03 20:00:13 -0300

    feat: limits post aspect ratio and width

--- apps/vite-project/src/components/Posts.tsx
+++ apps/vite-project/src/components/Posts.tsx
@@ -1,29 +1,33 @@
 import { Box, Typography, Avatar, Stack, IconButton } from "@mui/material";
 import LeafUpvote from "../assets/LeafUpvote.svg"
+import SaveIcon from "../assets/Save.svg"
 
 export default function Posts() {
     return (
-        <Box sx={{width: "100%", maxWidth: "500px", height: "350px", padding: "0px 20px 0px 20px", margin: "0 auto"}}>
+        <Box sx={{width: "100%", maxWidth: "380px", height: "350px", padding: "0px 20px 0px 20px", margin: "0 auto"}}>
             <Box>
                 <Stack direction="row" spacing={2} sx={{alignItems: "center", my: "5px"}}>
-                    <Avatar sx={{width: "50px", height: "50px", border: "2px solid #00C040"}} alt="foto-usuario"/>
+                    <Avatar sx={{width: "50px", height: "50px", border: "2px solid", borderColor: "primary.main"}} alt="foto-usuario"/>
                     <Typography sx={{fontSize: "14px", fontWeight: "600"}}>Nome do Autor</Typography>
                 </Stack>
             </Box>
-            <Box sx={{backgroundColor: "#F1F1F1", height: "100%", marginY: "8px"}}>
+            <Box sx={{backgroundColor: "#F1F1F1", height: "100%", my: "8px", aspectRatio: "1 / 1", mx: "auto"}}>
                 <Box sx={{aspectRatio: "1 / 1", objectFit: "contain"}}>imagem placeholder</Box>
             </Box>
             <Box>
                 <Stack direction="row" sx={{justifyContent: "space-between", alignItems: "center"}}>
                     <IconButton>
-                        <img style={{width: "50.37px", height: "53px"}} src={LeafUpvote}/>
+                        <Box component={"img"} style={{width: "50.37px", height: "53px"}} src={LeafUpvote}/>
                     </IconButton>
                     <Box>
-                        <IconButton>
-                            <img style={{width: "29.51px", height: "35.55px"}} src="./src/assets/chat.png"/>
+                    <IconButton>
+                            <Box component={"img"} style={{width: "39px"}} src={SaveIcon}/>
                         </IconButton>
                         <IconButton>
-                            <img style={{width: "30.72px", height: "32.64px"}} src="./src/assets/share.png"/>
+                            <Box component={"img"} style={{width: "29.51px", height: "35.55px"}} src="./src/assets/chat.png"/>
+                        </IconButton>
+                        <IconButton>
+                            <Box component={"img"} style={{width: "30.72px", height: "32.64px"}} src="./src/assets/share.png"/>
                         </IconButton>
                     </Box>
                 </Stack>
