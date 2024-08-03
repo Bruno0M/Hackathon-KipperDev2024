e68c9ebadb06e7404c22f5803c764979cc3ad980
Pyments <edupymota@protonmail.com>
2024-08-03 18:55:48 -0300

    feat: primitive bottom nav & header implemented

--- apps/vite-project/src/App.tsx
+++ apps/vite-project/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Login from "./components/Login";
+import NavHeader from "./components/NavHeader";
 
 function App() {
   return (
     <>
+      <NavHeader/>
       <Login />
     </>
   );
---
+++ apps/vite-project/src/components/BottomNav.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+export default function NavHeader() {
+  return (
+    <Box
+      component="header"
+      sx={{
+        width: "300px",
+        height: "230px",
+        border: "1px solid #D9D9D9",
+        borderRadius: "8px",
+        margin: "0 auto",
+        padding: "24px",
+        marginTop: "50px",
+      }}
+    >
+     
+    </Box>
+  );
+}
