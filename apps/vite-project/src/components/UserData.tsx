dad06c4d0f5abed605a9544b3dba0f8ff3447195
Lushlitz <pedhcavdsan1603@gmail.com>
2024-08-04 15:40:08 -0300

    feat: adicionando componente header

---
+++ apps/vite-project/src/components/UserData.tsx
@@ -0,0 +1,30 @@
+import { Box, Typography, Stack, IconButton } from "@mui/material";
+import { BrowserRouter as Routes, Route, useLocation } from 'react-router-dom';
+import close from "../assets/close.svg"
+import AvatarPlaceholder from "../assets/AvatarPlaceholder.svg"
+
+function HandleText() {
+  const location = useLocation();
+  return location.pathname 
+}
+
+export default function UserData() {
+    return (
+        <Box sx={{ width: "100%vw", padding: "0px 20px 0px 20px", backdropFilter: "blur(5px)",
+          backgroundColor: "#00C04041"}}>
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ justifyContent: "space-between", alignItems: "center", paddingY: "15px"}}
+        >
+          <IconButton>
+            <img src={close} alt="close button"/>
+          </IconButton>
+          <Typography sx={{ fontSize: "22px", fontWeight: "600" }}>
+          {HandleText().replace("/", "")} 
+          </Typography>
+          <img src={AvatarPlaceholder} alt="avatar"/>
+        </Stack>
+      </Box>
+    );
+}
