e78bde635c27ae397d02d1616864442eb8d15ab4
Lushlitz <pedhcavdsan1603@gmail.com>
2024-08-04 15:46:19 -0300

    renomeando UserData.tsx para Header.tsx

---
+++ apps/vite-project/src/components/Articles.tsx
@@ -0,0 +1,16 @@
+import { Box, Typography, Avatar, Stack } from "@mui/material";
+import UserData from "./UserData";
+import 
+export default function Articles() {
+    return (
+        <>
+            <UserData/>
+            <Box sx={{ backgroundColor: "#EFEBE7", padding: "0px 20px 0px 20px"}}>
+                <Typography>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis praesentium consequuntur eligendi expedita dicta labore consequatur. Corporis blanditiis iure corrupti praesentium nihil rerum sed repellat, culpa reprehenderit necessitatibus dolor ullam!
+                </Typography>
+
+            </Box>
+        </>
+    );
+}
