c3bf20c67320ab4554d819ce8c3263b2e0b79f68
Lushlitz <pedhcavdsan1603@gmail.com>
2024-08-03 21:31:39 -0300

    feat: adicionando o componente navBar

---
+++ apps/vite-project/src/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import { Box, Typography, Stack, IconButton } from "@mui/material";
+export default function NavBar() {
+    return (
+        <Box sx={{ width: "428px", height: "80px", backgroundColor: "#EFEBE7", alignContent: "center" }}>
+            <Stack direction="row" sx={{ justifyContent: "space-around" }}>
+                <IconButton>
+                    <img src="./src/assets/browse.svg"/>
+                </IconButton>
+                <IconButton>
+                    <img src="./src/assets/publish.svg"/>
+                </IconButton>
+                <IconButton>
+                    <img src="./src/assets/bell.svg"/>
+                </IconButton>
+            </Stack>
+            <Stack direction="row" sx={{ justifyContent: "space-around" }}>
+                <Typography sx={{ fontSize: "12px", fontWeight: "600"}}>Navegar</Typography>
+                <Typography sx={{ fontSize: "12px", fontWeight: "600" }}>Publicar</Typography>
+                <Typography sx={{ fontSize: "12px", fontWeight: "600" }}>Updates</Typography>
+            </Stack>
+        </Box>
+    );
+}
